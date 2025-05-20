ffedd72727d4e28a7c9069ebb33f669e04cf3ab3
Ercode Techoglu <ercode@pm.me>
2025-05-20 04:59:42 +0300

    add api link to homepage for quick access

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="h-screen w-screen space-y-4 p-10 text-center">
+	<p class="text-5xl">🇹🇷</p>
+	<h1 class="text-2xl opacity-70">U-Hilali</h1>
+	<a href="/api/v1/sensor" class="link link-primary">Reaktörden Gelen Anlık Sensör Bilgisi</a>
+</main>
